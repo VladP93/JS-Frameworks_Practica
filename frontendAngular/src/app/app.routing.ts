64a448b98fc19ce8bcd92e5241f14cb6d4e0a505
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-07-06 21:43:01 -0600

    routing

--- frontendAngular/src/app/app.routing.ts
+++ frontendAngular/src/app/app.routing.ts
@@ -3,3 +3,28 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //Importar componentes para página
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { ErrorComponent } from './components/error/error.component';
+
+//Array de rutas
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'peliculas', component: PeliculasComponent },
+  { path: 'pagina', component: PaginaComponent },
+  { path: 'pagina/:nombre', component: PaginaComponent },
+  { path: 'pagina/:nombre/:apellidos', component: PaginaComponent },
+  { path: '**', component: ErrorComponent },
+];
+
+//Exportar modulo de rutas
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(
+  appRoutes
+);
